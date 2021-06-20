b506450727d67ca581035850981258df44d50f25
JULIO AUGUSTO RIBEIRO DE SA ALMEIDA <julio.almeida9@fatec.sp.gov.br>
2021-06-20 19:42:27 -0300

    Recreating Department Controller and Views plus ef core by adding new scaffolded item, initial migration

--- WebSalesMVC/Models/Department.cs
+++ WebSalesMVC/Models/Department.cs
@@ -10,6 +10,10 @@ namespace WebSalesMVC.Models
         public int Id { get; set; }
         public string Name { get; set; }
 
+        public Department()
+        {
+        }
+
         public Department(int id, string name)
         {
             Id = id;
