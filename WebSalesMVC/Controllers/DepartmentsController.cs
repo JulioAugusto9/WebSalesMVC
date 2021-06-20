098a71465de9caf21ba3b3352e4cfa8bb7df38aa
JULIO AUGUSTO RIBEIRO DE SA ALMEIDA <julio.almeida9@fatec.sp.gov.br>
2021-06-20 17:59:55 -0300

    Iniciando Projeto MVC Web App, Model Department, DepartmentsController e Departments View

---
+++ WebSalesMVC/Controllers/DepartmentsController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebSalesMVC.Models;
+
+namespace WebSalesMVC.Controllers
+{
+    public class DepartmentsController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Department> deps = new List<Department>
+            {
+                new Department(1, "Sales"),
+                new Department(2, "Marketing"),
+                new Department(3, "Research and Development")
+            };
+            return View(deps);
+        }
+    }
+}
